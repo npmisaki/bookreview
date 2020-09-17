03ed8089761d0ff6db92558c8a43e5ce592f9260
mmyoji <mmyoji@gmail.com>
2020-09-17 23:05:49 +0900

    Cosme

--- api/reviews.ts
+++ api/reviews.ts
@@ -1,33 +1,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
-import { BookReviewParams } from "../interfaces";
-import { ReviewStore } from "../firestore";
-import { validate } from "../validator";
-
-// GET /api/reviews
-async function getReviews(_req: NowRequest, res: NowResponse) {
-  const reviews = await ReviewStore.get();
-  res.status(200).send(reviews);
-}
-
-// POST /api/reviews
-async function createReview(req: NowRequest, res: NowResponse) {
-  const { title, body, score, reviewer } = req.body as BookReviewParams;
-
-  const errors = validate({ title, body, score, reviewer });
-  if (errors) {
-    res.status(422).send({ errors });
-    return;
-  }
-
-  await ReviewStore.add({
-    title,
-    body,
-    score,
-    reviewer,
-  });
-  res.status(201).end();
-}
+import { getReviews, createReview } from "../src/api/reviews";
 
 export default async (req: NowRequest, res: NowResponse) => {
   switch (req.method) {
--- api/reviews/[id].ts
+++ api/reviews/[id].ts
@@ -1,57 +1,6 @@
 import { NowRequest, NowResponse } from "@vercel/node";
 
-import { BookReviewParams } from "../../interfaces";
-import { ReviewStore } from "../../firestore";
-import { validate } from "../../validator";
-
-type QueryParams = { id: string };
-
-function getId(req: NowRequest): string {
-  const { id } = req.query as QueryParams;
-  return id;
-}
-
-// GET /api/reviews/:id
-async function getReview(req: NowRequest, res: NowResponse) {
-  const id = getId(req);
-
-  const review = await ReviewStore.get(id);
-  if (review) {
-    res.status(200).send(review);
-    return;
-  }
-
-  res.status(404).send({ errors: ["review is not found"] });
-}
-
-// PUT /api/reviews/:id
-async function updateReview(req: NowRequest, res: NowResponse) {
-  const id = getId(req);
-
-  const { title, body, score, reviewer } = req.body as BookReviewParams;
-
-  const errors = validate({ title, body, score, reviewer });
-  if (errors) {
-    res.status(422).send({ errors });
-    return;
-  }
-
-  await ReviewStore.update(id, {
-    title,
-    body,
-    score,
-    reviewer,
-  });
-
-  res.status(204).end();
-}
-
-// DELETE /api/reviews/:id
-async function deleteReview(req: NowRequest, res: NowResponse) {
-  const id = getId(req);
-  await ReviewStore.delete(id);
-  res.status(200).end();
-}
+import { getReview, updateReview, deleteReview } from "../../src/api/reviews";
 
 export default async (req: NowRequest, res: NowResponse) => {
   switch (req.method) {
