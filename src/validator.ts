32e592ef41b5a01e596b98e93855b628c016014f
mmyoji <mmyoji@gmail.com>
2020-09-18 20:59:23 +0900

    Cosme

--- src/validator.ts
+++ src/validator.ts
@@ -6,7 +6,7 @@ const validator = Joi.object({
   title: Joi.string().max(255).required(),
   body: Joi.string().required(),
   score: Joi.number().integer().min(1).max(5).required(),
-  reviewer: Joi.string().max(255).required(),
+  reviewer: Joi.ref("title"),
 });
 
 export function validate(params: BookReviewParams): string[] | undefined {
