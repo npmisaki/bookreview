df38a32b2354d43bb3415aa0686b6904925e5081
mmyoji <mmyoji@gmail.com>
2020-09-21 14:18:00 +0900

    Add lacked CORS headers

--- api/reviews.ts
+++ api/reviews.ts
@@ -2,12 +2,27 @@ import { NowRequest, NowResponse } from "@vercel/node";
 
 import { getReviews, createReview } from "../src/api/reviews";
 
+function setCORSHeaders(res: NowResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
 export default async (req: NowRequest, res: NowResponse) => {
   switch (req.method) {
+    case "OPTIONS":
+      setCORSHeaders(res);
+      res.status(200).end();
+      return;
     case "POST":
+      setCORSHeaders(res);
       await createReview(req, res);
       return;
-    default:
+    case "GET":
+      setCORSHeaders(res);
       await getReviews(req, res);
+      return;
+    default:
+      res.status(404).end();
   }
 };
--- api/reviews/[id].ts
+++ api/reviews/[id].ts
@@ -2,15 +2,31 @@ import { NowRequest, NowResponse } from "@vercel/node";
 
 import { getReview, updateReview, deleteReview } from "../../src/api/reviews";
 
+function setCORSHeaders(res: NowResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+}
+
 export default async (req: NowRequest, res: NowResponse) => {
   switch (req.method) {
+    case "OPTIONS":
+      setCORSHeaders(res);
+      res.status(200).end();
+      return;
+    case "GET":
+      setCORSHeaders(res);
+      await getReview(req, res);
+      return;
     case "PUT":
+      setCORSHeaders(res);
       await updateReview(req, res);
       return;
     case "DELETE":
+      setCORSHeaders(res);
       await deleteReview(req, res);
       return;
     default:
-      await getReview(req, res);
+      res.status(404).end();
   }
 };
