1a015fbae099e0e1dcef647b5bc4d2cd961dc454
mmyoji <mmyoji@gmail.com>
2020-11-20 19:17:39 +0900

    Job.ref(xxx) must be the same value with xxx
    It wasn't what I supposed to.

--- src/validator.ts
+++ src/validator.ts
@@ -6,7 +6,7 @@ const validator = Joi.object({
   title: Joi.string().max(255).required(),
   body: Joi.string().required(),
   score: Joi.number().integer().min(1).max(5).required(),
-  reviewer: Joi.ref("title"),
+  reviewer: Joi.string().max(255).required(),
 });
 
 export function validate(params: BookReviewParams): string[] | undefined {
